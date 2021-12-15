a222012d11c8bbda6c645d76400675265fb2a9f6
Frojen <42121450+Frojen@users.noreply.github.com>
2021-12-15 11:01:25 +0300

    Update prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,11 @@
 from .base import *
+
 DEBUG = False
+
+SECRET_KEY = 'django-insecure-lk(u#t%wrk!_(q@*dn#!#l7z=pb6k+j^h8wwe2hdf%zb1+0b80'
+
 ALLOWED_HOSTS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
