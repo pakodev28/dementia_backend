c7d482f107dcc3248b480b11153e4f3d0c31800c
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-12-17 14:35:38 +0300

    Inherit Partner.Meta from DateMixin.Meta

--- demencia/models.py
+++ demencia/models.py
@@ -7,7 +7,6 @@ class Partner(DateMixin, PublishMixin):
     image = models.ImageField(upload_to='partners/', verbose_name='Изображение')
     name = models.CharField(max_length='250', verbose_name='Название партнёра')
 
-    class Meta:
-        orgering = ['-updated_at']
+    class Meta(DateMixin.Meta):
         verbose_name = 'Партнёр'
         verbose_name_plural = 'Партнёры'
