e6072c382c8a5fc25e673a218f1a690fcb8303aa
IgnatyevDmitriy <ignatyev-ds@yandex.ru>
2021-12-18 10:58:56 +0300

    Change verbose_names and url_label

--- demencia/admin.py
+++ demencia/admin.py
@@ -4,15 +4,15 @@ from .models import Partner, Slider
 
 
 class PartnerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url', 'image', 'is_active')
-    list_filter = ('name', 'is_active')
-    search_fields = ('name', )
+    list_display = ("name", "url", "image", "is_active")
+    list_filter = ("name", "is_active")
+    search_fields = ("name",)
 
 
 class SliderAdmin(admin.ModelAdmin):
-    list_display = ('title', 'text', 'image', 'url', 'is_active')
-    list_filter = ('title', 'is_active')
-    search_fields = ('title', 'text')
+    list_display = ("title", "text", "image", "url", "is_active")
+    list_filter = ("title", "is_active")
+    search_fields = ("title", "text")
 
 
 admin.site.register(Partner, PartnerAdmin)
--- demencia/models.py
+++ demencia/models.py
@@ -4,27 +4,27 @@ from core.mixins import DateMixin, PublishMixin
 
 
 class Partner(DateMixin, PublishMixin):
-    image = models.ImageField(upload_to='partners/', verbose_name='Изображение')
-    name = models.CharField(max_length=250, verbose_name='Название партнёра')
-    url = models.URLField(max_length=250, verbose_name='Ссылка')
+    image = models.ImageField(upload_to="partners/", verbose_name="Изображение")
+    name = models.CharField(max_length=250, verbose_name="Название партнёра")
+    url = models.URLField(max_length=250, verbose_name="Ссылка")
 
     class Meta(DateMixin.Meta):
-        verbose_name = 'Партнёр'
-        verbose_name_plural = 'Партнёры'
+        verbose_name = "Партнёр"
+        verbose_name_plural = "Партнёры"
 
     def __str__(self):
         return self.name
 
 
 class Slider(DateMixin, PublishMixin):
-    title = models.CharField(max_length=250, verbose_name='Заголовок')
-    image = models.ImageField(upload_to='slider/', verbose_name='Изображение')
-    url = models.URLField(max_length=250, verbose_name='Ссылка')
-    text = models.TextField(verbose_name='Текст')
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    image = models.ImageField(upload_to="slider/", verbose_name="Изображение")
+    url = models.URLField(max_length=250, verbose_name="Ссылка")
+    url_label = models.CharField(max_length=50, default="ПОДРОБНЕЕ", verbose_name="Название ссылки")
 
     class Meta(DateMixin.Meta):
-        verbose_name = 'Содержимое слайдера'
-        verbose_name_plural = 'Содержимое слайдера'
+        verbose_name = "Слайд"
+        verbose_name_plural = "Слайды"
 
     def __str__(self):
         return self.title
