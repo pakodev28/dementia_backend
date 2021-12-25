009cdb55ef09d7aa8332d3574f5aa4fb49e4cfec
Anton Lebedev <46112403+wrawka@users.noreply.github.com>
2021-12-25 22:31:11 +0300

    newsartice model upd

--- demencia/models.py
+++ demencia/models.py
@@ -1,19 +1,14 @@
-from solo.models import SingletonModel
-from tinymce.models import HTMLField
-
 from django.db import models
 
 from core.mixins import DateMixin, PublishMixin
 
 
 class NewsArticle(DateMixin, PublishMixin):
-    image = models.ImageField(upload_to="news/", verbose_name="Файл изображения")
-    title = models.CharField(max_length=250, verbose_name="Заголовок", help_text="Введите заголовок")
-    text = models.TextField(verbose_name="Текст новости", help_text="Введите текст")
-    url = models.URLField(max_length=250, verbose_name="Ссылка", help_text="Введите адрес ссылки")
-    url_label = models.CharField(
-        max_length=50, default="ПОДРОБНЕЕ", verbose_name="Название ссылки", help_text="Введите текст ссылки"
-    )
+    image = models.ImageField(upload_to="news/", verbose_name="Изображение")
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    text = models.TextField(verbose_name="Текст новости")
+    url = models.URLField(max_length=250, null=True, verbose_name="Ссылка")
+    url_label = models.CharField(max_length=50, default="ПОДРОБНЕЕ", verbose_name="Название ссылки")
 
     class Meta(DateMixin.Meta):
         verbose_name = "Новость"
@@ -37,9 +32,9 @@ class MapPoint(DateMixin, PublishMixin):
 
 
 class Partner(DateMixin, PublishMixin):
-    image = models.ImageField(upload_to="partners/", verbose_name="Файл изображения")
-    name = models.CharField(max_length=250, verbose_name="Название партнёра", help_text="Введите название партнёра")
-    url = models.URLField(max_length=250, verbose_name="Ссылка", help_text="Введите адрес ссылки")
+    image = models.ImageField(upload_to="partners/", verbose_name="Изображение")
+    name = models.CharField(max_length=250, verbose_name="Название партнёра")
+    url = models.URLField(max_length=250, verbose_name="Ссылка")
 
     class Meta(DateMixin.Meta):
         verbose_name = "Партнёр"
@@ -50,12 +45,10 @@ class Partner(DateMixin, PublishMixin):
 
 
 class Slider(DateMixin, PublishMixin):
-    title = models.CharField(max_length=250, verbose_name="Заголовок", help_text="Введите заголовок")
-    image = models.ImageField(upload_to="slider/", verbose_name="Файл изображения")
-    url = models.URLField(max_length=250, verbose_name="Ссылка", help_text="Введите адрес ссылки")
-    url_label = models.CharField(
-        max_length=50, default="ПОДРОБНЕЕ", verbose_name="Название ссылки", help_text="Введите текст ссылки"
-    )
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    image = models.ImageField(upload_to="slider/", verbose_name="Изображение")
+    url = models.URLField(max_length=250, verbose_name="Ссылка")
+    url_label = models.CharField(max_length=50, default="ПОДРОБНЕЕ", verbose_name="Название ссылки")
 
     class Meta(DateMixin.Meta):
         verbose_name = "Слайд"
@@ -87,38 +80,3 @@ class LeftMenuElement(DateMixin, PublishMixin):
 
     def __str__(self):
         return self.name
-
-
-class Settings(SingletonModel):
-    site_name = models.CharField(max_length=255, default="Деменция.net")
-    copyright = models.CharField(max_length=255, default="Благотворительный фонд «Память поколений»")
-    meta_description = models.TextField()
-
-    main_section_link = models.CharField(max_length=255, default="Пройти тест")
-    main_section_additional = models.TextField()
-
-    about_section = models.CharField(max_length=255, default="О деменции")
-    about_section_term = HTMLField()
-    about_section_term_link = models.CharField(max_length=255, default="Подробнее")
-    about_section_action_title = models.CharField(max_length=255, default="Помоги близким")
-    about_section_action_subtitle = models.CharField(max_length=255, default="Пройди тест с тем кому нужна помощь")
-    about_section_info = HTMLField()
-    about_section_link = models.CharField(max_length=255, default="Пройти тест")
-
-    news_section = models.CharField(max_length=255, default="Что нового?")
-    news_section_link = models.CharField(max_length=255, default="Перейти к ленте новостей")
-
-    partners_section = models.CharField(max_length=255, default="Кто с нами?")
-    partners_section_link = models.CharField(max_length=255, default="Партнеры")
-
-    map_section = models.CharField(max_length=255, default="Куда идти?")
-    map_section_link = models.CharField(max_length=255, default="Карта центров профилактики")
-    map_section_info = HTMLField()
-
-    fund_section = models.CharField(max_length=255, default="О фонде")
-    fund_section_info = HTMLField()
-    fund_section_link = models.CharField(max_length=255, default="Перейти на сайт фонда")
-
-    class Meta:
-        verbose_name = "Системные настройки"
-        verbose_name_plural = "Системные настройки"
