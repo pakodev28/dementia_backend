69e590353bfd843e053bb511ab4c0954b2e632e6
unknown <k0stya2007@yandex.ru>
2022-03-13 00:45:40 +0300

    validation special char in city and address

--- demencia/admin.py
+++ demencia/admin.py
@@ -1,4 +1,6 @@
+import re
 from html import unescape
+from phonenumber_field.formfields import PhoneNumberField
 
 from adminsortable2.admin import SortableAdminMixin
 from solo.admin import SingletonModelAdmin
@@ -198,7 +200,28 @@ class NewsArticleAdmin(admin.ModelAdmin):
         return mark_safe(f'<div style="overflow: auto; width:400px; height:100px;">{obj.text}</div>')
 
 
+class MapPointForm(forms.ModelForm):
+    class Meta:
+        model = MapPoint
+        fields = "__all__"
+
+    def clean_city(self):
+        """Проверяет отсутвие в поле город специальных символов и цифр"""
+        city = self.cleaned_data.get("city")
+        if re.search(r"[0-9@_!#$%^&*()<>?/\\|}{~:\[\]\.,]", city):
+            raise forms.ValidationError("Название города не может сожержать цифры и специальные символы")
+        return city
+
+    def clean_address(self):
+        """Проверяет отсутсвие в адресе специальных символов"""
+        address = self.cleaned_data.get("address")
+        if re.search(r"[@_!#$%^&*()<>?/\\|}{~:\[\]]", address):
+            raise forms.ValidationError("Адрес не может содержать специальные символы")
+        return address
+
+
 class MapPointAdmin(SortableAdminMixin, admin.ModelAdmin):
+    form = MapPointForm
     actions = [toggle_active, toggle_inactive]
     list_display = ("city", "region", "is_active", "address", "phone_no")
     list_filter = ("city", "is_active")
