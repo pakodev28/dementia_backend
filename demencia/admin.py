9e20d244d1644206b4303b9bea0f7f80b80a2f7b
unknown <k0stya2007@yandex.ru>
2022-03-13 01:03:30 +0300

    fix flake8

--- demencia/admin.py
+++ demencia/admin.py
@@ -1,6 +1,5 @@
 import re
 from html import unescape
-from phonenumber_field.formfields import PhoneNumberField
 
 from adminsortable2.admin import SortableAdminMixin
 from solo.admin import SingletonModelAdmin
