17cc175e7fef1220b0242fc8ca70d36068cc6ee3
Konstantin Zhmurko <k.zhmurko@factor-ts.ru>
2022-06-09 08:59:17 +0300

    add null field settings

--- config/settings.py
+++ config/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
